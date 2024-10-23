3efe86cb587f371646bd474477042163a61cf0fa
Starfosse <77751009+Starfosse@users.noreply.github.com>
2024-10-23 13:00:29 +0200

    Update index.ts
    ypdate
    
    update

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -1,32 +1,36 @@
-import React, { useContext, useState } from "react"
-import Toast from "../components/Toast"
-import { useQuery } from "react-query"
-import * as apiClient from "../api-client"
-import { loadStripe, Stripe } from "@stripe/stripe-js"
+import React, { useContext, useState } from "react";
+import Toast from "../components/Toast";
+import { useQuery } from "react-query";
+import * as apiClient from "../api-client";
+import { loadStripe, Stripe } from "@stripe/stripe-js";
 
-const STRIPE_PUB_KEY = import.meta.env.VITE_STRIPE_PUB_KEY || ""
+const STRIPE_PUB_KEY = import.meta.env.VITE_STRIPE_PUB_KEY || "";
 
 type ToastMessage = {
-  message: string
-  type: "SUCCESS" | "ERROR"
-}
+  message: string;
+  type: "SUCCESS" | "ERROR";
+};
 
 type AppContext = {
-  showToast: (toastMessage: ToastMessage) => void
-  isLoggedIn: boolean
-  stripePromise: Promise<Stripe | null>
-}
+  showToast: (toastMessage: ToastMessage) => void;
+  isLoggedIn: boolean;
+  stripePromise: Promise<Stripe | null>;
+};
 
-const AppContext = React.createContext<AppContext | undefined>(undefined)
+const AppContext = React.createContext<AppContext | undefined>(undefined);
 
-const stripePromise = loadStripe(STRIPE_PUB_KEY)
+const stripePromise = loadStripe(STRIPE_PUB_KEY);
 
-export const AppContextProdiver = ({ children }: { children: React.ReactNode }) => {
-  const [toast, setToast] = useState<ToastMessage | undefined>(undefined)
+export const AppContextProdiver = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [toast, setToast] = useState<ToastMessage | undefined>(undefined);
 
   const { isError } = useQuery("validateToken", apiClient.validateToken, {
     retry: false,
-  })
+  });
 
   return (
     <AppContext.Provider
@@ -34,7 +38,8 @@ export const AppContextProdiver = ({ children }: { children: React.ReactNode }) 
         showToast: (toastMessage) => setToast(toastMessage),
         isLoggedIn: !isError,
         stripePromise,
-      }}>
+      }}
+    >
       {toast && (
         <Toast
           message={toast.message}
@@ -44,10 +49,10 @@ export const AppContextProdiver = ({ children }: { children: React.ReactNode }) 
       )}
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 
 export const useAppContext = () => {
-  const context = useContext(AppContext)
-  return context as AppContext
-}
+  const context = useContext(AppContext);
+  return context as AppContext;
+};
